1da02bad14a7308213ada169962f0dbb7c258955
charson.leung <charsonleung673@gmail.com>
2025-04-10 22:01:34 +0800

    java_first

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    //I love java ("//": no impacts on coding)
+    //你好
+    
+    //System.out.printin is a tool
+    System.out.println("goodbye");
+  }
+}
