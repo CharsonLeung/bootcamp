3e7436a4a298f4598b15c1ae3ba9ef4b6d3bf260
charson.leung <charsonleung673@gmail.com>
2025-04-13 23:10:42 +0800

    Gute Nacht zzzzzzzzzz

--- demo-datatype/src/Exercise.java
+++ demo-datatype/src/Exercise.java
@@ -1,73 +1,43 @@
 public class Exercise {
   public static void main(String[] args) {
     System.out.println("Exercise!");
-    int a = 4;
-    int b = 6;
-    int c = a + b;
-    int d = (a + b) + c;
-    System.out.println(d);
-    double a1 = 5.12;
-    double a2 = 6.28;
-    System.out.println(a1 * a2);
-    double a3 = a1 + d;
-    System.out.println(a3);
-    double pi = 3.14159;
-    int n = 4;
-    double nPi = n * pi;
-    System.out.println("multiple of pi is ");
-    System.out.println(nPi);
-    double e;
-    e = 2.71828;
-    System.out.println(e);
-    System.out.println(pi / e);
-    char name;
-    name = 4;
-    System.out.println(name);
-    boolean logic;
-    logic = 1 < 2;
-    System.out.println(logic);
-    boolean logik = 3 < 2.9999;
-    System.out.println(logik);
-    byte bz;
-    bz = 1;
-    System.out.println(bz);
-    byte bzz = 102;
-    System.out.println(bzz);
 
-
-    double distKm = 21.0975 * 100;
-    System.out.println("Distance to run in km:");
-    System.out.println(distKm / 100);
-    int paceMin = 4 * 10;
-    System.out.println("pace in minutes:");
-    System.out.println(paceMin / 10);
-    double paceSec = 15;
-    System.out.println("pace in seconds:");
-    System.out.println(paceSec);
+    double distKm = 36 * 100;
+    System.out.println("Distance to run: " + distKm / 100 + "km");
+    //System.out.println(distKm / 100);
+    int paceMin = 3 * 10;
+    //System.out.println("pace in minutes:");
+    //System.out.println(paceMin / 10);
+    double paceSec = 57;
+    //System.out.println("pace in seconds:");
+    //System.out.println(paceSec);
+    System.out.println("Your pace: " + paceMin / 10 + " minutes " + paceSec + " seconds");
     double lengthMin = 6;
     double paceSecDeci;
     paceSecDeci = (paceSec / lengthMin);
     //System.out.println(paceSecDeci);
     double pace = paceMin + paceSecDeci;
     //System.out.println(pace);
-    System.out.println("your pace:");
-    System.out.println(paceMin / 10);
-    System.out.println("minute(s)");
-    System.out.println(paceSec);
-    System.out.println("second(s)");
+    //System.out.println("your pace:");
+    //System.out.println(paceMin / 10);
+    //System.out.println("minute(s)");
+    //System.out.println(paceSec);
+    //System.out.println("second(s)");
     double time = (distKm * pace) / 1000;
-    System.out.println("Your finishing time for the following distance in km:");
-    System.out.println(distKm / 100);
-    System.out.println("is as follow:");
+    
+    //System.out.println(distKm / 100);
+    //System.out.println("is as follow:");
     //System.out.println(time);
     
     int timeMin = (int)time;
-    System.out.println(timeMin);
-    System.out.println("minutes");
+    //System.out.println(timeMin);
+    //System.out.println("minutes");
     double timeSec = time * 10 - timeMin * 10;
     //System.out.println(timeSec);
     double timeSecR = timeSec * 6;
-    System.out.println((int)timeSecR);
-    System.out.println("seconds.");
+    //System.out.println((int)timeSecR);
+    //System.out.println("seconds.");
+    System.out.println("Your finishing time for " + distKm / 100 + "km is:");
+    System.out.println(timeMin + " minutes " + (int)timeSecR + " seconds.");
   }
 }
---
+++ demo-datatype/src/Exercise2.java
@@ -0,0 +1,56 @@
+public class Exercise2 {
+  public static void main(String[] args) {
+    int a = 4;
+    int b = 6;
+    int c = a + b;
+    int d = (a + b) + c;
+    System.out.println(d);
+    double a1 = 5.12;
+    double a2 = 6.28;
+    System.out.println(a1 * a2);
+    double a3 = a1 + d;
+    System.out.println(a3);
+    double pi = 3.14159;
+    int n = 4;
+    double nPi = n * pi;
+    System.out.println("multiple of pi is ");
+    System.out.println(nPi);
+    double e;
+    e = 2.71828;
+    System.out.println(e);
+    System.out.println(pi / e);
+    char name; //ASCII
+    name = 65;
+    char name2 = 66;
+    char name3 = 11 * 3 + 17 * 2;
+    System.out.println(name);
+    System.out.println(name2);
+    System.out.println(name3);
+    boolean logic;
+    logic = 1 < 2;
+    System.out.println(logic);
+    boolean logik = 3 < 2.9999;
+    System.out.println(logik);
+    byte bz;
+    bz = 1;
+    System.out.println(bz);
+    byte bzz = 102;
+    byte bzzz = 127;
+    System.out.println("try sth: " + bzz + bzzz);
+    System.out.println(bzz + bzzz);
+    long l3 = 12;
+    short s3 = 22;
+    System.out.println("else try: " + l3 + s3);
+    System.out.println(l3 + s3);
+    double d9 = 1.444444444444444444444;
+    System.out.println(d9);
+    float f3 = 9.8f;
+    float fg = 0.618f;
+    float sum = f3 + fg;
+    System.out.println(f3);
+    System.out.println(fg);
+    System.out.println(sum);
+
+
+  }  
+}
