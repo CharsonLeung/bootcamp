c72dc96ad7d8e2115cc5bb1025d856ef7c39b4d9
charson.leung <charsonleung673@gmail.com>
2025-05-03 00:44:14 +0800

    try new thing but almost forgot previous learned ones

---
+++ demo-class-object/src/BWV.java
@@ -0,0 +1,35 @@
+public class BWV {
+  private int number;
+  private String key;
+  private String inst;
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+  public void setKey(String key) {
+    this.key = key;
+  }
+  public void setInst(String inst) {
+    this.inst = inst;
+  }
+
+  public int getNumber() {
+    return this.number;
+  }
+  public String getKey() {
+    return this.key;
+  }
+  public String getInst() {
+    return this.inst;
+  }
+  public static void main(String[] args) {
+      // BWV 996 - BWV 1012
+    //String looper = "b";
+    int [] loop = new int[1012 - 996 + 1];
+    for (int i = 0; i < 1012 - 996 + 1; i++) {
+      loop[i] = i + 996;
+      System.out.println("bwv" + loop[i]);
+    // How to create new BWV with for loop, e.g. BWV bwvi = new BWV(); ?
+    }
+  }
+}
---
+++ demo-class-object/src/Marathoners.java
@@ -0,0 +1,91 @@
+import java.time.LocalDate;
+
+public class Marathoners {
+  private String name;
+  private String country;
+  private LocalDate birthDay;
+  private String personalRecord;
+  private int recordYear;
+  private int recordAge;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setBirthDay(LocalDate birthDay) {
+    this.birthDay = birthDay;
+  }
+  public void setCountry(String country) {
+    this.country = country;
+  }
+  public void setPersonalRecord(String personalRecord) {
+    this.personalRecord = personalRecord;
+  }
+  public void setRecordYear(int recordYear) {
+    this.recordYear = recordYear;
+  }
+  public void setRecordAge(int recordAge) {
+    this.recordAge = recordAge;
+  }
+  public String getName() {
+    return this.name;
+  }
+  public String getCountry() {
+    return this.country;
+  }
+  public LocalDate getBirthDay() {
+    return this.birthDay;
+  }
+  public String getPersonalRecord() {
+    return this.personalRecord;
+  }
+  public int getRecordYear() {
+    return this.recordYear;
+  }
+  public int getRecordAge() {
+    return this.recordAge;
+  }
+  public static void main(String[] args) {
+
+  Marathoners m1 = new Marathoners();
+  m1.setName("Haile Gebrselassie");
+  m1.setCountry("Ethiopia");
+  m1.setBirthDay(LocalDate.of(1973, 4, 18));
+  m1.setPersonalRecord("2:03:59");
+  m1.setRecordYear(2009);
+
+  Marathoners m2 = new Marathoners();
+  m2.setName("Sohn Kee-chung");
+  m2.setCountry("Japan(Korea)");
+  m2.setBirthDay(LocalDate.of(1912, 8, 29));
+  m2.setPersonalRecord("2:26:42");
+  m2.setRecordYear(1935);
+
+  Marathoners m3 = new Marathoners();
+  m3.setName("Samuel Wanjiru");
+  m3.setCountry("Kenya");
+  m3.setBirthDay(LocalDate.of(1986, 11, 10));
+  m3.setPersonalRecord("2:05:10");
+  m3.setRecordYear(2009);
+
+  Marathoners m4 = new Marathoners();
+  m4.setName("Eliud Kipchoge");
+  m4.setCountry("Kenya");
+  m4.setBirthDay(LocalDate.of(1984, 11, 05));
+  m4.setPersonalRecord("2:01:09");
+  m4.setRecordYear(2022);
+
+  Marathoners m5 = new Marathoners();
+  m5.setName("Kenenisa Bekele");
+  m5.setCountry("Ethiopia");
+  m5.setBirthDay(LocalDate.of(1982, 6, 13));
+  m5.setPersonalRecord("2:01:41");
+  m5.setRecordYear(2019);
+
+  // String q = m1;
+System.out.println("Name" + m2.getName() + 
+                   ", Country: " + m2.getCountry() + 
+                   ", Date of Birth: " + m2.getBirthDay() + 
+                   ", Personal Record: " + m2.getPersonalRecord() +
+                   ", Year of Record: " + m2.getRecordYear());
+  }
+}
